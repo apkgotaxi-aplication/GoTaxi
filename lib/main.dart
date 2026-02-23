46a43029171abcf89c58e257c4576b970ea19b7a
Alejandro Rios <alejandro.rios.cyt@gmail.com>
2026-02-23 10:30:33 +0100

    Login y Register

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import './presentation/screens/auth/auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Supabase credentials
+  await Supabase.initialize(
+    url: 'https://vkewprpynejnmobgpbiu.supabase.co',
+    anonKey: 'sb_publishable_0wl332igFmJN2iRCAJFqTg_KecNU5-b',
+  );
 
   runApp(const MyApp());
 }
@@ -10,80 +17,13 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // ... (resto de tu código)
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 39, 38, 41),
-        ),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _reducir() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 35, 72, 121),
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          const SizedBox(height: 8),
-          FloatingActionButton(
-            onPressed: _reducir,
-            tooltip: 'Reducir',
-            child: const Icon(Icons.reduce_capacity),
-          ),
-        ],
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Supabase Auth',
+      theme: ThemeData.dark(),
+      home: const AuthScreen(),
     );
   }
 }
