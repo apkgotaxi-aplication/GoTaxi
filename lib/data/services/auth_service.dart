e1813acc96291fd2d2031ed9ccf4fab197304df5
Alejandro Rios <alejandro.rios.cyt@gmail.com>
2026-03-19 17:49:03 +0100

    Auth Tests

---
+++ lib/data/services/auth_service.dart
@@ -0,0 +1,35 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract class AuthService {
+  Future<void> signIn({
+    required String email,
+    required String password,
+  });
+
+  Future<void> signUp({
+    required String email,
+    required String password,
+    required Map<String, dynamic> data,
+  });
+}
+
+class SupabaseAuthService implements AuthService {
+  SupabaseAuthService({GoTrueClient? authClient})
+      : _authClient = authClient ?? Supabase.instance.client.auth;
+
+  final GoTrueClient _authClient;
+
+  @override
+  Future<void> signIn({required String email, required String password}) {
+    return _authClient.signInWithPassword(email: email, password: password);
+  }
+
+  @override
+  Future<void> signUp({
+    required String email,
+    required String password,
+    required Map<String, dynamic> data,
+  }) {
+    return _authClient.signUp(email: email, password: password, data: data);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Supabase Auth',
+      title: 'GoTaxi',
       theme: ThemeData.dark(),
       home: const SplashScreen(),
     );
